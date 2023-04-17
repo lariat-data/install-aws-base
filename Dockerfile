8f4139e427c0e05bde36217e867a9068875ab63a
Aaditya Talwai <aaditya.21@gmail.com>
2023-04-17 01:08:51 +0100

    Update install-aws-base scripting

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
-FROM hashicorp/terraform:light
-RUN apk --no-cache add python3 py3-pip jq curl
+FROM --platform=linux/amd64 hashicorp/terraform:latest
+RUN apk --no-cache add python3 python3-dev py3-pip jq curl
 RUN pip3 install awscli boto3
 
 WORKDIR /workspace
--- scripts/kms/decrypt_and_store_remote_tfstate_profile.py
+++ scripts/kms/decrypt_and_store_remote_tfstate_profile.py
@@ -10,53 +10,11 @@ LARIAT_KEYS_ENC_FILE = "keys.enc"
 LARIAT_TERRAFORM_BUCKET_NAME = "lariat-customer-installation-tfstate"
 CROSS_ACCOUNT_ROLE_BASE_ARN = "arn:aws:iam::358681817243:role/lariat-iam-terraform-cross-account-access-role"
 
-def allow_caller_identity_role_access(account_id, kms_key_arn):
-    sts = boto3.client('sts')
-    arn = sts.get_caller_identity()["Arn"]
-
-    with open("policy_templates/iam_user_key_access.json.tpl", "r") as f:
-        src = Template(f.read())
-        result = src.substitute({
-            "customer_account_id": account_id,
-            "kms_key_arn": kms_key_arn
-        })
-
-def download_s3_keys_enc(customer_account_id):
-    session = boto3.Session()
-    s3_client = session.client('s3')
-    s3_client.download_file(LARIAT_TERRAFORM_BUCKET_NAME, f"{customer_account_id}/testkeys/{LARIAT_KEYS_ENC_FILE}", LARIAT_KEYS_ENC_FILE)
-
-def decrypt_s3_keys_enc(fileloc, customer_account_id):
-    # Define the AWS KMS client
-    kms_client = boto3.client('kms')
-
-    with open(fileloc, "rb") as encrypted_file:
-        encrypted_text = encrypted_file.read()
-        response = kms_client.decrypt(
-            CiphertextBlob=encrypted_text,
-            EncryptionContext={'user': 'terraform-user-' + customer_account_id}
-        )
-
-        decrypted_text = response['Plaintext']
-        with open(LARIAT_KEYS_FILE, "wb") as decrypted_file:
-            decrypted_file.write(decrypted_text)
-
 def get_and_decrypt_keypair(customer_account_id):
-    download_s3_keys_enc(customer_account_id)
-    decrypt_s3_keys_enc(LARIAT_KEYS_ENC_FILE, customer_account_id)
-
-    # Open the file containing the output
-    with open(LARIAT_KEYS_FILE, 'r') as f:
-        output = json.load(f)
-
-    # Parse the output to extract the access key ID and secret
-    access_key_id = output["aws_access_key_id"]
-    access_key_secret = output["aws_secret_access_key"]
-
     # Create a client for the STS service
     sts_client = boto3.client('sts',
-                            aws_access_key_id=access_key_id,
-                            aws_secret_access_key=access_key_secret,
+                            aws_access_key_id=os.environ["LARIAT_TMP_AWS_ACCESS_KEY_ID"],
+                            aws_secret_access_key=os.environ["LARIAT_TMP_AWS_SECRET_ACCESS_KEY"],
                             region_name='us-east-2'
         )
 
@@ -74,9 +32,6 @@ def get_and_decrypt_keypair(customer_account_id):
     # Convert the datetime object to string
     temp_creds['Expiration'] = temp_creds['Expiration'].strftime('%Y-%m-%dT%H:%M:%SZ')
     temp_creds['Expiration'] = str(temp_creds['Expiration'])
-
-    os.remove(LARIAT_KEYS_FILE)
-    os.remove(LARIAT_KEYS_ENC_FILE)
 
     print(json.dumps(temp_creds, indent=4))
 
