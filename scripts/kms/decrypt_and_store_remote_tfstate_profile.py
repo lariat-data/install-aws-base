034452f40b22fb4bd70cd54a9326f5e6c006d2ee
Aaditya Talwai <aaditya.21@gmail.com>
2023-04-17 22:11:34 +0100

    Reformat

--- scripts/kms/decrypt_and_store_remote_tfstate_profile.py
+++ scripts/kms/decrypt_and_store_remote_tfstate_profile.py
@@ -8,14 +8,19 @@ from string import Template
 LARIAT_KEYS_FILE = "keys.json"
 LARIAT_KEYS_ENC_FILE = "keys.enc"
 LARIAT_TERRAFORM_BUCKET_NAME = "lariat-customer-installation-tfstate"
-CROSS_ACCOUNT_ROLE_BASE_ARN = "arn:aws:iam::358681817243:role/lariat-iam-terraform-cross-account-access-role"
+CROSS_ACCOUNT_ROLE_BASE_ARN = (
+    "arn:aws:iam::358681817243:role/lariat-iam-terraform-cross-account-access-role"
+)
+
 
 def validate_aws_credentials(credentials, account_id):
     try:
-        sts_client = boto3.client("sts",
-                                  aws_access_key_id=credentials["aws_access_key_id"],
-                                  aws_secret_access_key=credentials["aws_secret_access_key"],
-                                  aws_session_token=credentials.get("aws_session_token"))
+        sts_client = boto3.client(
+            "sts",
+            aws_access_key_id=credentials["aws_access_key_id"],
+            aws_secret_access_key=credentials["aws_secret_access_key"],
+            aws_session_token=credentials.get("aws_session_token"),
+        )
     except (ClientError, KeyError):
         return False
 
@@ -31,32 +36,32 @@ def validate_aws_credentials(credentials, account_id):
     else:
         return False
 
+
 def get_and_decrypt_keypair(customer_account_id):
     # Create a client for the STS service
-    sts_client = boto3.client('sts',
-                            aws_access_key_id=os.environ["LARIAT_TMP_AWS_ACCESS_KEY_ID"],
-                            aws_secret_access_key=os.environ["LARIAT_TMP_AWS_SECRET_ACCESS_KEY"],
-                            region_name='us-east-2'
-        )
+    sts_client = boto3.client(
+        "sts",
+        aws_access_key_id=os.environ["LARIAT_TMP_AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key=os.environ["LARIAT_TMP_AWS_SECRET_ACCESS_KEY"],
+        region_name="us-east-2",
+    )
 
     # Assume the cross-account role
     role_arn = f"{CROSS_ACCOUNT_ROLE_BASE_ARN}-{customer_account_id}"
     session_name = "terraform-s3-session-" + customer_account_id
-    response = sts_client.assume_role(
-        RoleArn=role_arn,
-        RoleSessionName=session_name
-    )
+    response = sts_client.assume_role(RoleArn=role_arn, RoleSessionName=session_name)
 
     # Extract the temp credentials from the assumed role
-    temp_creds = response['Credentials']
+    temp_creds = response["Credentials"]
 
     # Convert the datetime object to string
-    temp_creds['Expiration'] = temp_creds['Expiration'].strftime('%Y-%m-%dT%H:%M:%SZ')
-    temp_creds['Expiration'] = str(temp_creds['Expiration'])
+    temp_creds["Expiration"] = temp_creds["Expiration"].strftime("%Y-%m-%dT%H:%M:%SZ")
+    temp_creds["Expiration"] = str(temp_creds["Expiration"])
 
     print(json.dumps(temp_creds, indent=4))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     aws_account_id = sys.argv[1]
 
     aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
@@ -72,9 +77,9 @@ if __name__ == '__main__':
         credentials["aws_session_token"] = aws_session_token
 
     valid = validate_aws_credentials(credentials, aws_account_id)
-    if valid:
-        print(f"Successfully validated credentials for AWS account ID {aws_account_id}")
-    else:
-        sys.exit(f"The provided AWS credentials are not valid for the specified AWS account ID: {aws_account_id}")
+    if not valid:
+        sys.exit(
+            f"The provided AWS credentials are not valid for the specified AWS account ID: {aws_account_id}"
+        )
 
     get_and_decrypt_keypair(aws_account_id)
